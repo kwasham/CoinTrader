81d02245765b894cf6ce5824ef3d97423d85d870
kwasham <kwashamdev@gmail.com>
2023-08-05 16:06:59 -0500

    top-ten-url-update

--- src/pages/index.js
+++ src/pages/index.js
@@ -33,7 +33,7 @@ export default Page;
 export async function getServerSideProps() {
   try {
     console.log("Fetching top ten data...");
-    const response = await fetch("http://localhost:3000/api/get-top-ten");
+    const response = await fetch("https://d8f7-24-27-36-117.ngrok-free.app/api/get-top-ten");
     const data = await response.json();
     console.log("Here is the top ten data: ", data);
     return {
